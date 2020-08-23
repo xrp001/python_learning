85746eb31eccfac3fd1566e386d4513cce3a8d63
xrp001 <xiongrongpeng@gmail.com>
2020-08-23 21:42:58 +0800

    [UPDATE] 添加内容

--- python001.py
+++ python001.py
@@ -1,2 +1,16 @@
 # coding:utf-8
 
+a = 100 + 200
+print a
+print 'hello,world'
+print 'The quick brown fox', 'jumps over', 'the lazy dog'
+
+print '100 + 200 =', 100 + 200
+
+# name = raw_input()
+# print name
+
+print 'I\'m ok'
+
+a = True
+print (type(a))
