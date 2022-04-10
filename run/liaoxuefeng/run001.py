848283c893dea8bfe83168def13c14219091e157
xrp001 <xiongrongpeng@gmail.com>
2022-04-10 13:42:50 +0800

    update

--- run/liaoxuefeng/run001.py
+++ run/liaoxuefeng/run001.py
@@ -4,9 +4,9 @@ from fun.liaoxuefeng.fun001 import power
 a = 100 + 200
 print (a)
 print ('hello,world')
-print 'The quick brown fox', 'jumps over', 'the lazy dog'
+print ('The quick brown fox', 'jumps over', 'the lazy dog')
 
-#####################################
+####################################
 print '100 + 200 =', 100 + 200
 
 #####################################
