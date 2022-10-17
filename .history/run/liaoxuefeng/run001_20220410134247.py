4f761ca3792a9d825a390a4f0a1d2c0672a7b6e1
Chris Xiong <xiongrongpeng@gmail.com>
2022-10-18 01:57:04 +0800

    记录历史

---
+++ .history/run/liaoxuefeng/run001_20220410134247.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+from fun.liaoxuefeng.fun001 import power
+
+a = 100 + 200
+print (a)
+print ('hello,world')
+print ('The quick brown fox', 'jumps over', 'the lazy dog')
+
+####################################
+print '100 + 200 =', 100 + 200
+
+#####################################
+# name = raw_input()
+# print name
+
+#####################################
+print 'I\'m ok'
+
+#####################################
+a = True
+print (type(a))
+
+#####################################
+print '''line1
+line2
+line3'''
+
+#####################################
+b = 123
+print b, type(b)
+b = 'ABC'
+print b, type(b)
+
+#####################################
+x = 10
+x = x + 2
+print x
+
+#####################################
+print power(5, 3)
+
+#####################################
+aa = u"厉害了"
+bb = u"厉害了"
+cc = u"不厉害"
+for i in [aa, bb, cc]:
+    print type(i)
+
+print aa == bb
+print aa in [bb]
+print aa == cc
+print aa in [cc]
+print aa == u"厉害了"
+print aa in [u"不厉害"]
